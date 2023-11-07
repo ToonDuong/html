378593a346406be8f920a997803911c1f8f5907e
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-07 21:56:39 +0700

    SLide25

---
+++ PHP/slide25.php
@@ -0,0 +1,10 @@
+<?php
+$Subject = "Lập trình ứng dụng web";
+$unversityName = "HUMG";
+$firstNumber = 2;
+$secondtNumber = 2;
+
+echo "Học " . $Subject . "ở phòng 221 tại trường " . $unversityName . "<br>";
+echo "Học $Subject ở phòng 221 tại trường $unversityName <br>";
+echo "<h1> $unversityName </h1>";
+echo $firstNumber + $secondtNumber;
