c64baa6e7910bb319ecc96d14a32570f311fe84a
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-07 21:30:25 +0700

    Slide19

--- PHP/slide19.php
+++ PHP/slide19.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltf">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            echo "MY FIRST PHP FILE";
-        ?>
-        <h3>Hello World!</h3>
-        <select>
-            <option>1</option>
-            <option>2</option>
-        </select>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+    <?php
+    echo "MY FIRST PHP FILE";
+    ?>
+    <h3>Nguyễn Tôn Dương</h3>
+    <select>
+        <option>1</option>
+        <option>2</option>
+    </select>
+</body>
+
 </html>
