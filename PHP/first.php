aeaf42b8071983c615b45deda74fbb8093f7fe6d
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-07 21:21:56 +0700

    firstphp

--- PHP/first.php
+++ PHP/first.php
@@ -1,7 +1,8 @@
 <?php
-    echo "MY FIRST PHP FILE";
-    echo "<h3>Nguyễn Tôn Dương - 2121050379<h3>";
-    echo "<select>";
-    echo "<option>1</option>";
-    echo "<option>2</option>";
-    echo "</select>";
+echo "MY FIRST PHP FILE"
+echo "<h3>Nguyễn Tôn Dương - 2121050379<h3>";
+echo "<select>";
+echo "<option>1</option>";
+echo "<option>2</option>";
+echo "</select>";
+?>
