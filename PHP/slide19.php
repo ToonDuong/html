62b983c4d6441c3f214b9d0c7a108acee02698af
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-07 21:01:14 +0700

    first den slide 24

---
+++ PHP/slide19.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltf">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "MY FIRST PHP FILE";
+        ?>
+        <h3>Hello World!</h3>
+        <select>
+            <option>1</option>
+            <option>2</option>
+        </select>
+    </body>
+</html>
---
+++ PHP/slide24.php
@@ -0,0 +1,8 @@
+<?php
+    $longAccount = 10.000.000;
+    echo "So tien trong tai khoan Long: $longAccount";
+    $HUMGPay = 10.000.000;
+    echo "HUMG chuyen tien: $HUMGPay";
+    $longAccount = $longAccount + $HUMGPay;
+    echo "So tien trong tai khoang Long: $longAccount";
+?>
