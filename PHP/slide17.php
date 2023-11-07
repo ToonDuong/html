ba5461c0579ba43eb487c23f055b480be70bc773
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-07 21:24:27 +0700

    slide17

--- PHP/slide17.php
+++ PHP/slide17.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+
     <head>
-        <meta charset="utf-8">
-        <title></title>
-    <head>
+
     <body>
         <?php
-            echo "MY FIRST PHP FILE";
+        echo "MY FIRST PHP FILE";
         ?>
-        <h3>Hello World!</h3>
+        <h3>Nguyễn Tôn Dương</h3>
         <select>
             <option>1</option>
             <option>2</option>
         </select>
     </body>
+
 </html>
