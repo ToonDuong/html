f6e5d44251ed6229ed7e7714c49b976746500177
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-08 15:31:26 +0700

    slide34

---
+++ PHP/slide34.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+    <form action="5-formGet.php" method="get">
+        <p>Name: </p>
+        <input type="text" name="name"><br>
+        <p?>E-mail: </p>
+            <input type="text" name="email"><br>
+            <p></p>
+            <input type="submit">
+
+    </form>
+</body>
+
+</html>
