559bc0777870e8479d68e3124f1d357ffdc0e9fb
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-08 16:02:06 +0700

    Slide41

---
+++ PHP/slide41.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+    <form action="6-formPost.php" method="post">
+        <p>Name :</p>
+        <input type="text" name="name"><br>
+        <p>E-mail :</p>
+        <input type="text" name="email"><br>
+        <p></p>
+        <input type="submit">
+
+    </form>
+</body>
+
+</html>
