4b806659aa6a280cadb9124e4425ecd58c054b67
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-21 16:02:18 +0700

    slide8

---
+++ PHP2/slide8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Nguyen Ton Duong - 2121050379</h2>
+    <h3>QUIT THE GAME</h3>
+    <form action="./8-ifresult.php" method="get">
+        <select name="response" id="">
+            <option value="yes">YES</option>
+            <option value="no">NO</option>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
