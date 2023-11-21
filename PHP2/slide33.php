e83304c1911a02e36864d9dab816d4e505394308
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-21 16:36:28 +0700

    slide33

---
+++ PHP2/slide33.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Nguyen Ton Duong - 2121050379</title>
+
+</head>
+
+<body>
+
+    <?php
+    $exchangeRate = array("USD" => 22300, "EUR" => 27300, "SGD" => 17000, "JPY" => 120);
+    $amount = $_GET['amount'];
+    $currency = $_GET["currency"];
+    echo " $amount USD is equal " . number_format($amount * $exchangeRate[$currency]) . " VND";
+    ?>
+
+</body>
+
+</html>
