29f705618bb6d71ffcb5a590e8ce987e11e2df65
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-21 16:25:27 +0700

    Slide26

---
+++ PHP2/sldie26.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<?php
+$exchangeRate = array(22300, 27300, 17000, 120);
+echo "<h3>Exchange rate to VND</h3>";
+echo "USD: $exchangeRate[0]<br>";
+echo "EUR: $exchangeRate[1]<br>";
+echo "SGD: $exchangeRate[2]<br>";
+echo "JPY: $exchangeRate[3]<br>";
+?>
+
+</html>
