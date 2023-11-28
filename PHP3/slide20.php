2be591bff9dae4befa076beb53eee930e38f3109
ToonDuong <nguyentonduong2k3@gmail.com>
2023-11-28 18:53:24 +0700

    slide20

---
+++ PHP3/slide20.php
@@ -0,0 +1,13 @@
+<?php
+$items = array('item1.png', 'item2.png');
+foreach ($items as $item) {
+    echo "<img src='images/$item' width = 50px height = 50px alt='$item'>";
+}
+?>
+<h1>function</h1>
+<?php
+$items = array('item3.png', 'item4.png');
+foreach ($items as $item) {
+    echo "<img src ='images/$item' width = 50px height = 50px alt='$item'>";
+}
+?>
