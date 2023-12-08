22a958e35ce1a1bfc801ffdf1cf0ebefda6f3871
ToonDuong <nguyentonduong2k3@gmail.com>
2023-12-08 15:17:35 +0700

    slide68

---
+++ PHPL3-2/slide68.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <title>SEARCH FLIGHT</title>
+    <link rel="stylesheet" type="text/css" href="4-search.css">
+</head>
+
+<body>
+    <h1> Search Flight </h1>
+    <form method="get" action="" name="register">
+        Origin <input type="text" name="origin" /><br>
+        Destination <input type="text" name="destination" /><br>
+        <input type="submit" value="Search" /> <br>
+    </form>
+
+    <?php
+    if (isset($_GET['origin'])) {
+        $origin = $_GET['origin'];
+        $destination = $_GET['destination'];
+        require 'connect.php';
+        mysqli_set_charset($conn, 'UTF8');
+        $sql = "SELECT * FROM flights WHERE origin = '$origin' and destination='$destination'";
+        $result = $conn->query($sql);
+        require('4-displayFlight.php');
+        $conn->close();
+    }
+    ?>
+
+</body>
+
+</html>
